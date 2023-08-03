83e2e74da8b6eaf7d22426c8bd2da71216bd1444
DianYangFu <dianyangfu@gmail.com>
2023-08-03 11:29:03 +0800

    feat(epl): adjust `article-image`, support vertical image

--- packages/election2024-president-letters/components/shared/article-image.tsx
+++ packages/election2024-president-letters/components/shared/article-image.tsx
@@ -11,15 +11,30 @@ const pictureCoverCSS = css`
   aspect-ratio: 4 / 3;
   padding-top: 75%; //fallback for aspect-ratio
 `
-const Picture = styled.picture<{ type: 'cover' | 'content' }>`
+const pictureContentCSS = css<{ shouldObjectFitContain: boolean }>`
+  position: relative;
+  display: block;
+  width: 100%;
+  height: auto;
+  aspect-ratio: 4 / 3;
+  padding-top: 75%; //fallback for aspect-ratio
+  img {
+    object-fit: ${({ shouldObjectFitContain }) =>
+      shouldObjectFitContain ? 'contain' : 'initial'};
+  }
+`
+const Picture = styled.picture<{
+  type: 'cover' | 'content'
+  shouldObjectFitContain: boolean
+}>`
   ${({ type }) => {
     switch (type) {
       case 'cover':
         return pictureCoverCSS
       case 'content':
-        return null //todo
+        return pictureContentCSS
       default:
-        return null //todo
+        return pictureContentCSS
     }
   }}
 `
@@ -35,38 +50,43 @@ type CoverImageProps = {
     tabletWebP: string
     mobileWebP: string
   }
+  imageDirection?: 'horizontal' | 'vertical'
 }
 
 export default function ArticleImage({
   name,
   type = 'content',
   imagesSrc,
+  imageDirection = 'horizontal',
 }: CoverImageProps): JSX.Element {
+  const shouldObjectFitContain = imageDirection === 'vertical'
   return (
-    <Picture type={type}>
-      <source
-        srcSet={imagesSrc.desktopWebP}
-        media="(min-width: 1200px)"
-        type="image/webP"
-      />
-      <source
-        srcSet={imagesSrc.desktop}
-        media="(min-width: 1200px)"
-        type="image/jpeg"
-      />
-      <source
-        srcSet={imagesSrc.tabletWebP}
-        media="(min-width: 768px)"
-        type="image/webP"
-      />
-      <source
-        srcSet={imagesSrc.tablet}
-        media="(min-width: 768px)"
-        type="image/jpeg"
-      />
-      <source srcSet={imagesSrc.mobileWebP} type="image/webP" />
+    <>
+      <Picture type={type} shouldObjectFitContain={shouldObjectFitContain}>
+        <source
+          srcSet={imagesSrc.desktopWebP}
+          media="(min-width: 1200px)"
+          type="image/webP"
+        />
+        <source
+          srcSet={imagesSrc.desktop}
+          media="(min-width: 1200px)"
+          type="image/jpeg"
+        />
+        <source
+          srcSet={imagesSrc.tabletWebP}
+          media="(min-width: 768px)"
+          type="image/webP"
+        />
+        <source
+          srcSet={imagesSrc.tablet}
+          media="(min-width: 768px)"
+          type="image/jpeg"
+        />
+        <source srcSet={imagesSrc.mobileWebP} type="image/webP" />
 
-      <Image src={imagesSrc.mobile} fill alt={name} />
-    </Picture>
+        <Image alt={name} src={imagesSrc.mobile} fill></Image>
+      </Picture>
+    </>
   )
 }
