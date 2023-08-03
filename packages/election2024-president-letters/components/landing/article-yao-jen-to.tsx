9eda122a8accde89cabb24952b57a66519b9f728
DianYangFu <dianyangfu@gmail.com>
2023-08-03 11:30:00 +0800

    feat(epl): show article content in YaoJeoTo

--- packages/election2024-president-letters/components/landing/article-yao-jen-to.tsx
+++ packages/election2024-president-letters/components/landing/article-yao-jen-to.tsx
@@ -1,4 +1,10 @@
 import ArticleCover from './article-cover'
+import ArticleContent from '../shared/article-content'
 export default function ArticleYaoJeoTo() {
-  return <ArticleCover></ArticleCover>
+  return (
+    <article>
+      <ArticleCover></ArticleCover>
+      <ArticleContent></ArticleContent>
+    </article>
+  )
 }
