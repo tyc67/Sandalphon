aafe6a75b29e192495ba9eacf72191f758585fb9
DianYangFu <dianyangfu@gmail.com>
2023-08-04 16:51:42 +0800

    Revert "style(epl): temporarily unshow header, adjust style of scroll-snap"
    This reverts commit 3c6b8265f57a8493b80a99093697ca21ca67f78c.

--- packages/election2024-president-letters/components/landing/article-yao-jen-to.tsx
+++ packages/election2024-president-letters/components/landing/article-yao-jen-to.tsx
@@ -3,6 +3,7 @@ import ArticleContent from '../shared/article-content'
 import { content } from '../../constants/article/yao-jen-to'
 import styled from 'styled-components'
 const Article = styled.article`
+  overflow: auto;
   scroll-snap-align: start;
   width: 100vw;
   height: 100vh;
