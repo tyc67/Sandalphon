1ab7815e1cbde69b4ec4e5c7e42311b063e8885b
Tsuki Akiba <erase2004@gmail.com>
2023-08-08 11:48:12 +0800

    refactor(epl): add optionApiUrl config

--- packages/election2024-president-letters/config/index.ts
+++ packages/election2024-president-letters/config/index.ts
@@ -3,6 +3,7 @@ const projectName: string = String(process.env.NEXT_PUBLIC_PROJECT_NAME)
 let feedbackFormId = process.env.NEXT_PUBLIC_FEEDBACK_FORM_ID ?? ''
 let emotionFieldId = process.env.NEXT_PUBLIC_EMOTION_FIELD_ID ?? ''
 let textFieldId = process.env.NEXT_PUBLIC_TEXT_FIELD_ID ?? ''
+let optionApiUrl = process.env.NEXT_PUBLIC_OPTION_API_URL ?? ''
 let staticFileDestination: string
 let protocol = 'http'
 let host = 'localhost'
@@ -45,4 +46,5 @@ export {
   feedbackFormId,
   emotionFieldId,
   textFieldId,
+  optionApiUrl,
 }
