351f96d6646ea6e8292210cc781651e4f32f9a98
caesar <caesar.lee@mirrormedia.mg>
2023-08-09 16:53:33 +0800

    refactor(ccc-uk-v3): update page data

--- packages/2023-ccc-uk-v3/pages/_document.js
+++ packages/2023-ccc-uk-v3/pages/_document.js
@@ -15,26 +15,20 @@ function CustomDocument() {
 
         <meta property="og:locale" content="zh_TW" />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="凝視頓巴斯：張乾琦的戰地攝影紀實" />
+        <meta property="og:title" content="逆襲" />
         <meta
           property="og:image"
-          content="https://www.mirrormedia.mg/assets/images/20230208110214-84ac8a1a28b780a2b6231d78f87c44b4-tablet.jpg"
+          content="https://www.mirrormedia.mg/assets/images/20230725193655-cd77bed5a5193c7bdd80a82d36e6cce3-tablet.jpg"
         />
         <meta
           property="og:image:secure_url"
-          content="https://www.mirrormedia.mg/assets/images/20230208110214-84ac8a1a28b780a2b6231d78f87c44b4-tablet.jpg"
+          content="https://www.mirrormedia.mg/assets/images/20230725193655-cd77bed5a5193c7bdd80a82d36e6cce3-tablet.jpg"
         />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta
-          property="og:description"
-          content="過去一年，攝影家張乾琦先後四次進入烏克蘭拍攝，本刊繼去年獨家取得授權，這次將刊載第四次、在烏東頓巴斯地區的影像作品，這裡被視為戰爭決戰點，也是目前兩軍激戰最激烈的地方之一。"
-        />
+        <meta property="og:description" content="張乾琦烏東戰地攝影紀實" />
         <meta property="og:url" content={ogUrl} />
         <meta property="fb:app_id" content="175313259598308" />
-        <meta
-          name="description"
-          content="過去一年，攝影家張乾琦先後四次進入烏克蘭拍攝，本刊繼去年獨家取得授權，這次將刊載第四次、在烏東頓巴斯地區的影像作品，這裡被視為戰爭決戰點，也是目前兩軍激戰最激烈的地方之一。"
-        />
+        <meta name="description" content="張乾琦烏東戰地攝影紀實" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
