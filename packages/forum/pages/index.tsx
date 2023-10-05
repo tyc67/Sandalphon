9cc8bccec0a272f47df501e6acdf4b89ff2eef73
Xuan <as840922qq@gmail.com>
2023-10-05 11:05:53 +0800

    feat(forum): add `NavListsProvider`

--- packages/forum/pages/index.tsx
+++ packages/forum/pages/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 import { breakpoint, color } from '~/styles/theme'
-import type { ForumData } from '~/types'
+import type { ForumData, NavItem } from '~/types'
 import { OG_DESC, OG_TITLE, bgImageURL, JSON_URL } from '~/config'
 
 import Layout from '~/components/layout/layout'
@@ -15,6 +15,8 @@ import Schedule from '~/components/schedule'
 import Registration from '~/components/registration'
 import RelatedPost from '~/components/related-post'
 import Partners from '~/components/partners'
+import { staticFileDestination } from '~/config'
+import { NavListsProvider } from '~/contexts/nav-list'
 
 const Main = styled.main`
   background-image: ${bgImageURL ? `url(${bgImageURL})` : 'none'};
@@ -64,9 +66,55 @@ export default function Home(): JSX.Element {
   const videoSrc = pageInfo?.video?.content || ''
   const speakersData = speakers || []
   const registrationText = pageInfo?.registration?.content || ''
+  const partnersData = partners || {}
+
+  // 檢查是否顯示 header 的「共同推動」項目：確認 `partners` 是否為空物件 + image 是否皆為空字串
+  const isPartnerAllEmpty = Object.values(partnersData).every((partners) => {
+    return partners.every((partner) => partner.image === '')
+  })
+  const shouldShowPartners =
+    Object.keys(partnersData).length !== 0 && !isPartnerAllEmpty
+
+  const navLists: NavItem[] = [
+    {
+      title: '論壇簡介',
+      href: `${staticFileDestination}#introduction`,
+      show: !(introQrCodeSrc === '' && introText === ''),
+    },
+    {
+      title: '活動影音',
+      href: `${staticFileDestination}#video`,
+      show: Boolean(videoSrc),
+    },
+    {
+      title: '與會陣容',
+      href: `${staticFileDestination}#speakers`,
+      show: Boolean(speakers?.length),
+    },
+    {
+      title: '論壇議程',
+      href: `${staticFileDestination}#schedule`,
+      show: Boolean(schedule?.length),
+    },
+    {
+      title: '相關報導',
+      href: `${staticFileDestination}#related-post`,
+      show: Boolean(relatedPost?.length),
+    },
+    {
+      title: '報名資訊',
+      href: `${staticFileDestination}#registration`,
+      show: Boolean(registrationText),
+    },
+    {
+      title: '共同推動',
+      href: `${staticFileDestination}#partners`,
+      show: shouldShowPartners,
+    },
+  ]
 
   return (
-    <>
+    <NavListsProvider navLists={navLists}>
       <CustomHead
         title={OG_TITLE}
         description={OG_DESC}
@@ -82,9 +130,9 @@ export default function Home(): JSX.Element {
           <Schedule content={schedule} />
           <RelatedPost relatedPosts={relatedPost} />
           <Registration content={registrationText} />
-          <Partners partners={partners} />
+          <Partners partners={partnersData} />
         </Main>
       </Layout>
-    </>
+    </NavListsProvider>
   )
 }
