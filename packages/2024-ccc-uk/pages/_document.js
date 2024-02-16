d4f54ecd867e17c8a920f018a94f3f641e8974f4
caesar <caesar.lee@mirrormedia.mg>
2024-02-16 17:15:02 +0800

    refactor(ccc-uk-2024): update meta

--- packages/2024-ccc-uk/pages/_document.js
+++ packages/2024-ccc-uk/pages/_document.js
@@ -5,8 +5,8 @@ import { environment } from '../const'
 function CustomDocument() {
   const ogUrl =
     environment === 'dev'
-      ? 'https://www.mirrormedia.mg/projects/ccc_ukraine_202308-dev/index.html'
-      : 'https://www.mirrormedia.mg/projects/ccc_ukraine_202308/index.html'
+      ? 'https://www.mirrormedia.mg/projects/dev-ccc_ukraine_2024/index.html'
+      : 'https://www.mirrormedia.mg/projects/ccc_ukraine_2024/index.html'
 
   return (
     <Html>
@@ -16,7 +16,7 @@ function CustomDocument() {
         <meta property="og:type" content="article" />
         <meta
           property="og:title"
-          content="逆襲：張乾琦烏東戰地攝影紀實/Resistance"
+          content="莫忘烏克蘭：張乾琦．烏俄戰爭兩週年戰地紀實/Ukraine: Lest We Forget"
         />
         <meta
           property="og:image"
@@ -29,17 +29,19 @@ function CustomDocument() {
         <meta property="og:image:type" content="image/jpeg" />
         <meta
           property="og:description"
-          content="今年三月，攝影師張乾琦動了攝護腺癌手術。休養不久，他馬上穿上重達九公斤的第四級防彈板背心，每天快走四、五公里鍛鍊體能。手術後僅僅三個月，他再度走訪烏克蘭，死神和砲火都止不住他凝視真實、報導真相的熱情。<br>In March of this year, photographer Chien-Chi Chang underwent prostate cancer surgery. After a brief recovery, he started walking regularly with 9-kilogram bulletproof vest equipped with level-four armored plates for four to five kilometers every day to regain stamina. Just three months after his surgery, he embarked on another journey to Ukraine, neither death nor artillery could deter his passion for capturing reality and reporting the truth."
+          content="你還記得布查大屠殺嗎？戰爭的殘忍景象曾讓全球民主國家憤慨，各國政要紛紛表達支持烏克蘭。兩年過去了，不少西方國家已經產生戰爭疲勞，質疑為何繼續資助這場戰爭的聲音愈來愈高；一般人的悲憫之情也在消退，對戰爭畫面已逐漸無感。
+          張乾琦始終沒有忘記烏克蘭。他在文章裡說，當奧地利人以高空煙火慶祝新年時，烏克蘭人卻在爆炸聲中驚醒。他冒著嚴寒浴雪而行，第八次前往烏克蘭戰地，見證前線「絞肉機」般漫長艱苦的戰況。"
         />
         <meta property="og:url" content={ogUrl} />
         <meta property="fb:app_id" content="175313259598308" />
         <meta
           name="description"
-          content="今年三月，攝影師張乾琦動了攝護腺癌手術。休養不久，他馬上穿上重達九公斤的第四級防彈板背心，每天快走四、五公里鍛鍊體能。手術後僅僅三個月，他再度走訪烏克蘭，死神和砲火都止不住他凝視真實、報導真相的熱情。<br>In March of this year, photographer Chien-Chi Chang underwent prostate cancer surgery. After a brief recovery, he started walking regularly with 9-kilogram bulletproof vest equipped with level-four armored plates for four to five kilometers every day to regain stamina. Just three months after his surgery, he embarked on another journey to Ukraine, neither death nor artillery could deter his passion for capturing reality and reporting the truth."
+          content="你還記得布查大屠殺嗎？戰爭的殘忍景象曾讓全球民主國家憤慨，各國政要紛紛表達支持烏克蘭。兩年過去了，不少西方國家已經產生戰爭疲勞，質疑為何繼續資助這場戰爭的聲音愈來愈高；一般人的悲憫之情也在消退，對戰爭畫面已逐漸無感。
+          張乾琦始終沒有忘記烏克蘭。他在文章裡說，當奧地利人以高空煙火慶祝新年時，烏克蘭人卻在爆炸聲中驚醒。他冒著嚴寒浴雪而行，第八次前往烏克蘭戰地，見證前線「絞肉機」般漫長艱苦的戰況。"
         />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700;900&display=swap"
           rel="stylesheet"
