1a8ed732f3c503ee0929a0b57ea685c5677b5d14
caesar <caesar.lee@mirrormedia.mg>
2024-03-09 16:29:12 +0800

    style(318): set box-sizing: border-box

--- packages/2024-318-sticky-notes/pages/_app.js
+++ packages/2024-318-sticky-notes/pages/_app.js
@@ -1,9 +1,17 @@
 import { Provider } from 'react-redux'
 import store from '../store'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GlobalStyle />
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
