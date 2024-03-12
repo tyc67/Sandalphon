e09adcc821f635f6dd7d70026e4fa35e10364560
Xuan <as840922qq@gmail.com>
2024-03-12 21:04:35 +0800

    fix(318): modify page structure

--- packages/2024-318-sticky-notes/pages/_app.js
+++ packages/2024-318-sticky-notes/pages/_app.js
@@ -1,19 +1,18 @@
 import { Provider } from 'react-redux'
 import store from '../store'
-import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-`
+import { ThemeProvider } from 'styled-components'
+import { theme } from '~/styles/theme'
+import { GlobalStyles } from '~/styles/global-styles'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <GlobalStyles />
       <Provider store={store}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     </>
   )
