597b8f4df5ab2b21fbdba7b916c215e47e56d497
Xuan <as840922qq@gmail.com>
2024-03-12 21:03:56 +0800

    feat(318): add wide-article configs

---
+++ packages/2024-318-sticky-notes/config/index.js
@@ -0,0 +1,32 @@
+const env = String(process.env.NEXT_PUBLIC_ENV)
+const projectName = String(process.env.NEXT_PUBLIC_PROJECT_NAME)
+
+let SITE_URL = ''
+let READR_URL = ''
+let staticFileDestination = ''
+
+switch (env) {
+  case 'dev':
+    SITE_URL = 'https://dev-next.mirrormedia.mg'
+    READR_URL = 'https://v3-dev.readr.tw'
+    staticFileDestination = `https://www.mirrormedia.mg/projects/${projectName}`
+
+    break
+
+  case 'prod': {
+    SITE_URL = 'https://www.mirrormedia.mg'
+    READR_URL = 'https://www.readr.tw'
+    staticFileDestination = `https://www.mirrormedia.mg/projects/${projectName}`
+
+    break
+  }
+  default: {
+    SITE_URL = 'https://dev-next.mirrormedia.mg'
+    READR_URL = 'https://v3-dev.readr.tw'
+    staticFileDestination = `http://localhost:3000`
+
+    break
+  }
+}
+
+export { staticFileDestination, SITE_URL, READR_URL }
