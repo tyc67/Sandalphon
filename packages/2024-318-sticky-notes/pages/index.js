a6ca9fba65491f34241861410c91f4b041184e80
Xuan <as840922qq@gmail.com>
2024-03-13 11:00:36 +0800

    fix(318): prettier error

--- packages/2024-318-sticky-notes/pages/index.js
+++ packages/2024-318-sticky-notes/pages/index.js
@@ -5,7 +5,6 @@ import PostData from '../data/post-data.json'
 import PostContent from '../data/post-content.json'
 
 export default function Home() {
-
   const data = Object.assign(PostData)
   const content = Object.assign(PostContent)
 
