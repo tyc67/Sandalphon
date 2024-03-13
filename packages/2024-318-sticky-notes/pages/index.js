b18c90e41715bf8be238fdaaa24392ff5c136bcf
Xuan <as840922qq@gmail.com>
2024-03-13 21:56:12 +0800

    fix(318): get data from JSON

--- packages/2024-318-sticky-notes/pages/index.js
+++ packages/2024-318-sticky-notes/pages/index.js
@@ -1,17 +1,40 @@
+import axios from 'axios'
+
 import TransformContainer from '../components/sticky-notes/TransformContainer'
-import WideStyleArticle from '../components/wide-article/index'
-// article mock data
-import PostData from '../data/post-data.json'
-import PostContent from '../data/post-content.json'
+import WideStyleArticle from '~/components/wide-article/index'
 
-export default function Home() {
-  const data = Object.assign(PostData)
-  const content = Object.assign(PostContent)
+import { POST_JSON } from '~/const/wide-article'
 
+export default function Home({ postData, postContent }) {
   return (
     <>
-      <WideStyleArticle postData={data} postContent={content} />
+      <WideStyleArticle postData={postData} postContent={postContent} />
       <TransformContainer />
     </>
   )
+}
+
+export async function getStaticProps() {
+  let postData = null
+  let postContent = null
+
+  try {
+    const response = await axios.get(`${POST_JSON}`)
+
+    const posts = response.data
+
+    postData = posts?.post || null
+    postContent = postData.content
+      ? { type: 'fullContent', data: postData.content, isLoaded: true }
+      : null
+  } catch (error) {
+    console.error('Error fetching post data:', error)
+  }
+
+  return {
+    props: {
+      postData,
+      postContent,
+    },
+  }
 }
