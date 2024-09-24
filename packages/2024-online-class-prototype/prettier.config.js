9865ecd9a2829a821ee2116db83175700b2c6409
Tsuki Akiba <erase2004@gmail.com>
2024-09-24 15:31:56 +0800

    chore(online-class): set up environment

---
+++ packages/2024-online-class-prototype/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.ts',
+}
