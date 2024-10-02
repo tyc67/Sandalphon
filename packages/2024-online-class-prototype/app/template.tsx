da61baee651df2285fa190433ba67a41386bc232
Tsuki Akiba <erase2004@gmail.com>
2024-10-02 10:17:29 +0800

    feat(online-class): fetch signed cookie

--- packages/2024-online-class-prototype/app/template.tsx
+++ packages/2024-online-class-prototype/app/template.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { COLLECTION_NAME } from '@/constants/config'
-import { selectUid } from '@/redux/features/user/selector'
+import { selectToken, selectUid } from '@/redux/features/user/selector'
 import {
   resetAll,
   setIsLogined,
@@ -18,11 +18,29 @@ import {
   onSnapshot,
   type Unsubscribe,
 } from 'firebase/firestore'
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren, useCallback, useEffect } from 'react'
 
 export default function Template({ children }: PropsWithChildren) {
   const dispatch = useAppDispatch()
   const uid = useAppSelector(selectUid)
+  const token = useAppSelector(selectToken)
+
+  const fetchSignedCookie = useCallback(
+    async (givenToken?: string) => {
+      try {
+        const jsonBody = { id_token: givenToken ?? token }
+
+        await fetch('/auth/verify_token', {
+          method: 'POST',
+          body: JSON.stringify(jsonBody),
+        })
+      } catch (error) {
+        console.error('// Encountered error while retrieving signed cookie //')
+        console.error(error)
+      }
+    },
+    [token]
+  )
 
   useEffect(() => {
     const auth = getAuth(firebaseApp)
@@ -34,13 +52,14 @@ export default function Template({ children }: PropsWithChildren) {
         dispatch(setUid(user.uid))
         dispatch(setToken(idToken))
         dispatch(setPurchasedClassIDs(classIDs))
+        fetchSignedCookie(idToken)
       } else {
         dispatch(resetAll())
       }
     })
 
     return () => unsubscribe()
-  }, [dispatch])
+  }, [dispatch, fetchSignedCookie])
 
   useEffect(() => {
     const auth = getAuth(firebaseApp)
@@ -48,13 +67,14 @@ export default function Template({ children }: PropsWithChildren) {
       if (user) {
         const idToken = await user.getIdToken()
         dispatch(setToken(idToken))
+        fetchSignedCookie(idToken)
       } else {
         dispatch(resetAll())
       }
     })
 
     return () => unsubscribe()
-  }, [dispatch])
+  }, [dispatch, fetchSignedCookie])
 
   useEffect(() => {
     const store = getFirestore(firebaseApp)
@@ -65,6 +85,7 @@ export default function Template({ children }: PropsWithChildren) {
         async (doc) => {
           const classIDs = (await doc.data()?.courses) ?? []
           dispatch(setPurchasedClassIDs(classIDs))
+          fetchSignedCookie()
         }
       )
     }
@@ -72,7 +93,7 @@ export default function Template({ children }: PropsWithChildren) {
     return () => {
       if (typeof unsubscribe === 'function') unsubscribe()
     }
-  }, [dispatch, uid])
+  }, [dispatch, uid, fetchSignedCookie])
 
   return children
 }
