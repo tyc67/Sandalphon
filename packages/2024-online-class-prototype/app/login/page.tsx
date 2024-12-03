779ca6c43061cfe8ceb84929914f63e06dcbce78
Tsuki Akiba <erase2004@gmail.com>
2024-12-03 17:57:37 +0800

    feat(online-class): add LoginMethodSelector

--- packages/2024-online-class-prototype/app/login/page.tsx
+++ packages/2024-online-class-prototype/app/login/page.tsx
@@ -10,8 +10,20 @@ import { useAppSelector } from '@/redux/hooks'
 import { selectIsLogined } from '@/redux/features/user/selector'
 import SMSLoginForm from './_components/sms-login-form'
 import EmailLoginForm from './_components/emaill-login-form'
+import LoginMethodSelector from './_components/login-method-selector'
 
 const RETRY_PERIOD = 1000 * 30 // 30 seconds
+
+const LoginMethod = {
+  SMS: {
+    name: '手機登入',
+    value: 'sms',
+  },
+  Email: {
+    name: 'email登入',
+    value: 'email',
+  },
+}
 
 export default function Page() {
   const captchaContainerRef = useInitFirebasePhoneCaptcha()
@@ -22,6 +34,7 @@ export default function Page() {
     ORIGIN_STORAGE_KEY,
     ''
   )
+  const [loginMethod, setLoginMethod] = useState(LoginMethod.SMS.value)
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -38,11 +51,32 @@ export default function Page() {
     }
   }, [router, isLogined, removeOriginPath])
 
+  const onSelectorClicked = (method: string) => {
+    setLoginMethod(method)
+  }
+
   return (
-    <div className="my-auto flex flex-col items-center gap-y-10">
-      <SMSLoginForm now={now} retryPeriod={RETRY_PERIOD} />
-      <EmailLoginForm now={now} retryPeriod={RETRY_PERIOD} />
-      <div className="hidden" ref={captchaContainerRef}></div>
+    <div className="my-auto flex flex-col items-center">
+      <div className="mb-5 space-x-3">
+        {Object.entries(LoginMethod).map(([key, value]) => (
+          <LoginMethodSelector
+            key={key}
+            text={value.name}
+            method={value.value}
+            isActive={loginMethod === value.value}
+            onClick={onSelectorClicked}
+          />
+        ))}
+      </div>
+      <div className="flex flex-col items-center gap-y-10">
+        {loginMethod === LoginMethod.SMS.value && (
+          <SMSLoginForm now={now} retryPeriod={RETRY_PERIOD} />
+        )}
+        {loginMethod === LoginMethod.Email.value && (
+          <EmailLoginForm now={now} retryPeriod={RETRY_PERIOD} />
+        )}
+        <div className="hidden" ref={captchaContainerRef}></div>
+      </div>
     </div>
   )
 }
